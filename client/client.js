0460006e10343a9bd6c97fe65dae49a25d8fa94e
Lili Dworkin <lilidworkin@gmail.com>
2015-06-23 17:41:26 +0000

    oh boy

--- client/client.js
+++ client/client.js
@@ -1,9 +1,3 @@
-Template.wrapper.helpers({
-    isTurk: function() {
-	return this.params.workerId;
-    }
-});
-
 Template.main.helpers({
     active: function() {
 	if (Meteor.loggingIn()) {
@@ -22,6 +16,22 @@ Accounts.ui.config({
 
 Meteor.startup(function () {
     $('.alert').alert()
-    Meteor.subscribe('users');
     Meteor.subscribe('games');
+    Meteor.subscribe('users', function() {
+	var params = Router.current().params.query;
+	var wid = params.workerId;
+	var user = Meteor.user();
+	if (!wid) {
+	    Meteor.logout();
+	}
+	if (wid && (!user || user.username != wid)) {
+	    var result = Meteor.users.findOne({'username': params.workerId});
+	    if (!result) {
+		Accounts.createUser({'username': params.workerId, 
+				     'password': params.workerId});		    
+	    } else {
+		Meteor.loginWithPassword(params.workerId, params.workerId);
+	    }
+	}
+    });
 });
