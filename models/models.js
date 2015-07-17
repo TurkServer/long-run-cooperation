85b85c701a14fad3c8026773299d2fb0a14c96f9
Lili Dworkin <lilidworkin@gmail.com>
2015-07-17 19:00:41 +0000

    added gamegroups collection

--- models/models.js
+++ models/models.js
@@ -2,6 +2,7 @@ Rounds = new Mongo.Collection('rounds');
 Games = new Mongo.Collection('games'); 
 Recruiting = new Mongo.Collection('recruiting'); 
 Sessions = new Mongo.Collection('sessions'); // one per assignment (user/HIT)
+GameGroups = new Mongo.Collection('gamegroups'); // one per next-game initiated
 TurkServer.partitionCollection(Rounds);
 TurkServer.partitionCollection(Games);
 TurkServer.partitionCollection(Recruiting);
