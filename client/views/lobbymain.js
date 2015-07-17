fd1d28bd9ffdda80e4cdc788f5ba211def021e10
Lili Dworkin <lilidworkin@gmail.com>
2015-07-17 15:43:08 +0000

    we're painting the buttons blue

---
+++ client/views/lobbymain.js
@@ -0,0 +1,22 @@
+Template.lobbyMain.helpers({
+    'notReady': function() {
+	var obj = LobbyStatus.findOne(Meteor.userId());
+	return obj && !obj.status;
+    },
+    'numPlayers': function() {
+	return numPlayers;
+    },
+    'numWaiting': function() {
+	return LobbyStatus.find({'status': true}).count();
+    },
+    'plural': function() {
+	var count = LobbyStatus.find({'status': true}).count();
+	return count > 1;
+    }
+});
+
+Template.lobbyMain.events({
+    'click .begin': function() {
+	Meteor.call('toggleStatus');
+    }
+});
