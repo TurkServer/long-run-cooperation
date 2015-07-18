28979e495515b0b6b29850e9b6186c3af38fd20e
Lili Dworkin <lilidworkin@gmail.coom>
2015-07-18 14:46:37 -0400

    cleaning batches

--- server/server.js
+++ server/server.js
@@ -7,25 +7,15 @@ Meteor.publish('sessions', function(userId) {
 }); 
 
 Meteor.startup(function () {
-    Batches.upsert({name: 'testing'}, {name: 'testing', active: true});
-    Batches.upsert({name: 'pilot'}, {name: 'pilot', active: true});
+    Batches.upsert({name: 'main'}, {name: 'main', active: true});
     Batches.upsert({name: 'recruiting'}, {name: 'recruiting', active: true});
-    Batches.upsert({name: 'recruiting-testing'}, {name: 'recruiting-testing', active: true});
 
     TurkServer.ensureTreatmentExists({name: 'main'});
     TurkServer.ensureTreatmentExists({name: 'recruiting'});
 
-    var batchid = Batches.findOne({name: 'testing'})._id;
+    var batchid = Batches.findOne({name: 'main'})._id;
     TurkServer.Batch.getBatch(batchid).setAssigner(new TurkServer.Assigners.PairAssigner);
-    Batches.update({name: 'testing'}, {$addToSet: {treatments: 'main'}});
-
-    var batchid = Batches.findOne({name: 'recruiting-testing'})._id;
-    TurkServer.Batch.getBatch(batchid).setAssigner(new TurkServer.Assigners.SimpleAssigner);
-    Batches.update({name: 'recruiting-testing'}, {$addToSet: {treatments: 'recruiting'}});
-
-    var batchid = Batches.findOne({name: 'pilot'})._id;
-    TurkServer.Batch.getBatch(batchid).setAssigner(new TurkServer.Assigners.PairAssigner);
-    Batches.update({name: 'pilot'}, {$addToSet: {treatments: 'main'}});
+    Batches.update({name: 'main'}, {$addToSet: {treatments: 'main'}});
 
     var batchid = Batches.findOne({name: 'recruiting'})._id;
     TurkServer.Batch.getBatch(batchid).setAssigner(new TurkServer.Assigners.SimpleAssigner);
