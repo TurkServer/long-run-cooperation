2a77a5311c616403fc7bb9a21dc5450291a751b3
Lili Dworkin <lilidworkin@gmail.com>
2015-07-20 19:19:26 +0000

    analysis functions

--- analyze.py
+++ analyze.py
@@ -8,29 +8,33 @@ db = client.meteor
 
 # pilot things
 
-games = [["FeFfLSSazQALHYWGm", "ReZMKmPcqyNJmAHbd", "erT5isCghEnGfZ7gw"],
-         ["NB9QGdWASMA8zfbLR", "kDmAr96wBwzQhppSQ", "krurFiTsktX9wxbj7"],
-         ["zRDDfrSKHB5kocnjf", "5ReHP6ZKePN5zpqm5"],
-         ["AY43MC6HWA5m63Qfo", "2ivQLzvgxKmPXAgux"],
-         ["PeWCziYMquY5xwrF2", "PZiqsCuy6SMohJJsG"],
-         ["TBd2HHtoLD7HRmzvP", "vrNPPrde5LgPjXkFt"],
-         ["G3wiJnchnKBvmsWdo", "3q3CStNLzzfPeqZWz"],
-         ["tSi7ZS8MvdoTSunGc", "Ge88kj9LPwEAXfsMQ"],
-         ["uXexr5scHgWq2AAJW", "NrfignX3hskLxFGAy"],
-         ["drzeTT6nLyzxBbsj6"],
-         ["n3hX58rGNva94xAJD"],
-         ["mS6uYHsFp7uv7QpFJ"],
-         ["4rSmc5NczaotDuMY4"],
-         ["EbvbziYNHx338cmAh"],
-         ["x8hWBug3PEGfSWdbe"],
-         ["js6L2MjvpZYcQR8on"],
-         ["KhojGEY8qRY7Piby6"],
-         ["ZKHKqs7hcXjdxWrMt"],
-         ["ovHEKNB44mRXxQfBf"],
-         ["7Cqn2edHfiJnNnGwB"]]
+pilotGames = [["FeFfLSSazQALHYWGm", "ReZMKmPcqyNJmAHbd", "erT5isCghEnGfZ7gw"],
+              ["NB9QGdWASMA8zfbLR", "kDmAr96wBwzQhppSQ", "krurFiTsktX9wxbj7"],
+              ["zRDDfrSKHB5kocnjf", "5ReHP6ZKePN5zpqm5"],
+              ["AY43MC6HWA5m63Qfo", "2ivQLzvgxKmPXAgux"],
+              ["PeWCziYMquY5xwrF2", "PZiqsCuy6SMohJJsG"],
+              ["TBd2HHtoLD7HRmzvP", "vrNPPrde5LgPjXkFt"],
+              ["G3wiJnchnKBvmsWdo", "3q3CStNLzzfPeqZWz"],
+              ["tSi7ZS8MvdoTSunGc", "Ge88kj9LPwEAXfsMQ"],
+              ["uXexr5scHgWq2AAJW", "NrfignX3hskLxFGAy"],
+              ["drzeTT6nLyzxBbsj6"],
+              ["n3hX58rGNva94xAJD"],
+              ["mS6uYHsFp7uv7QpFJ"],
+              ["4rSmc5NczaotDuMY4"],
+              ["EbvbziYNHx338cmAh"],
+              ["x8hWBug3PEGfSWdbe"],
+              ["js6L2MjvpZYcQR8on"],
+              ["KhojGEY8qRY7Piby6"],
+              ["ZKHKqs7hcXjdxWrMt"],
+              ["ovHEKNB44mRXxQfBf"],
+              ["7Cqn2edHfiJnNnGwB"]]
+
+
+def mean(l):
+    return float(sum(l))/len(l)
 
 def active_users():
-    num = [len(game) for game in games]
+    num = [len(game) for game in pilotGames]
     plt.plot(range(1,21), num)
     plt.ylabel('Number of game instances launched')
     plt.xlabel('Game counter')
@@ -42,24 +46,55 @@ def active_users():
 
 problems = ["PZiqsCuy6SMohJJsG", "Ge88kj9LPwEAXfsMQ", "x8hWBug3PEGfSWdbe", "js6L2MjvpZYcQR8on"]
     
-def investigate(id):
-    for round_ in sorted(db.rounds.find({'_groupId': id}), key=lambda x: x['timestamp']):
+def investigate(instanceId):
+    for round_ in sorted(db.rounds.find({'_groupId': instanceId}), key=lambda x: x['timestamp']):
         print 'Round: ' + str(round_['roundIndex'])
         print 'User: ' + round_['userId']
         print 'Timestamp: ' + str(round_['timestamp'])
         print 'Payoff: ' + str(round_.get('payoff'))
         print
+
+
+def plotOne():
+    coops = coopWrapper()
+    gameCoops = [[mean(coops.get(instanceId, [1])) for instanceId in games] for games in pilotGames]
+    return [mean(x) for x in gameCoops]
+
+def plotTwo():
+    roundCoops = [[] for _ in range(10)]
+    coops = coopWrapper()
+    for games in pilotGames:
+        gameGroupRoundCoop = [[] for _ in range(10)]
+        for instanceId in games:
+            roundCoop = coops.get(instanceId, numpy.ones(10))
+            for roundIndex, coop in enumerate(roundCoop):
+                gameGroupRoundCoop[roundIndex].append(coop)
+        gameGroupRoundCoop = [mean(x) for x in gameGroupRoundCoop]
+        for roundIndex, l in enumerate(roundCoops):
+            roundCoops[roundIndex].append(gameGroupRoundCoop[roundIndex])
+    return roundCoops
+
+
+            
         
-        
-def getGame(_id):
-    roundObjs = sorted(db.rounds.find({'_groupId': gameid}), key=lambda x: x['roundIndex'])
-    rounds = []
-    for index, actions in itertools.groupby(roundObjs, key=lambda x: x['roundIndex']):
-        rounds.append(list(actions))
+def coopWrapper():
+    instances = list(db.ts.experiments.find())
+    coops = {}
+    for instance in instances:
+        instanceId = instance['_id']
+        game = db.games.find_one({'_groupId': instanceId})
+        if game['round'] == 10:
+            coops[instanceId] = gameCoop(instanceId)
+    return coops
+
+def gameCoop(instanceId):
+    roundObjs = sorted(db.rounds.find({'_groupId': instanceId}), key=lambda x: x['roundIndex'])
+    rounds = [list(actions) for _, actions in itertools.groupby(roundObjs, key=lambda x: x['roundIndex'])]
+    roundCoop = []
     for index, actions in enumerate(rounds):
-        print 'Round %d' % index
-        for action in sorted(actions, key=lambda x: x['userId']):
-            print '%s: %d' % (action['userId'], action['action'])
+        coops = sum([action['action'] == 1 for action in actions])
+        roundCoop.append(float(coops)/2)
+    return roundCoop
 
 def payment():
     games = list(db.ts.experiments.find())
