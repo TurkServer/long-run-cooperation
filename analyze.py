9fc81b129ffca5793174a3ded12ae760624f2192
Lili Dworkin <lilidworkin@gmail.com>
2015-07-20 22:48:41 +0000

    analysis scripts

--- analyze.py
+++ analyze.py
@@ -2,68 +2,48 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import matplotlib.pyplot as plt
 import itertools
+import random
+import numpy
 
 client = MongoClient()
 db = client.meteor
-
-# pilot things
-
-pilotGames = [["FeFfLSSazQALHYWGm", "ReZMKmPcqyNJmAHbd", "erT5isCghEnGfZ7gw"],
-              ["NB9QGdWASMA8zfbLR", "kDmAr96wBwzQhppSQ", "krurFiTsktX9wxbj7"],
-              ["zRDDfrSKHB5kocnjf", "5ReHP6ZKePN5zpqm5"],
-              ["AY43MC6HWA5m63Qfo", "2ivQLzvgxKmPXAgux"],
-              ["PeWCziYMquY5xwrF2", "PZiqsCuy6SMohJJsG"],
-              ["TBd2HHtoLD7HRmzvP", "vrNPPrde5LgPjXkFt"],
-              ["G3wiJnchnKBvmsWdo", "3q3CStNLzzfPeqZWz"],
-              ["tSi7ZS8MvdoTSunGc", "Ge88kj9LPwEAXfsMQ"],
-              ["uXexr5scHgWq2AAJW", "NrfignX3hskLxFGAy"],
-              ["drzeTT6nLyzxBbsj6"],
-              ["n3hX58rGNva94xAJD"],
-              ["mS6uYHsFp7uv7QpFJ"],
-              ["4rSmc5NczaotDuMY4"],
-              ["EbvbziYNHx338cmAh"],
-              ["x8hWBug3PEGfSWdbe"],
-              ["js6L2MjvpZYcQR8on"],
-              ["KhojGEY8qRY7Piby6"],
-              ["ZKHKqs7hcXjdxWrMt"],
-              ["ovHEKNB44mRXxQfBf"],
-              ["7Cqn2edHfiJnNnGwB"]]
 
 
 def mean(l):
     return float(sum(l))/len(l)
 
+
 def active_users():
-    num = [len(game) for game in pilotGames]
+    gamegroups = sorted(db.gamegroups.find(), key=lambda x: x['counter'])
+    num = [len(game['users']) for game in gamegroups]
     plt.plot(range(1,21), num)
-    plt.ylabel('Number of game instances launched')
+    plt.ylabel('Number of active users')
     plt.xlabel('Game counter')
-    plt.ylim((0,4))
     plt.xlim((1,20))
     plt.xticks(range(1,21))
-    plt.yticks(range(5))
+    plt.ylim((1,22))
+    plt.yticks(range(22))
     plt.show()
-
-problems = ["PZiqsCuy6SMohJJsG", "Ge88kj9LPwEAXfsMQ", "x8hWBug3PEGfSWdbe", "js6L2MjvpZYcQR8on"]
-    
-def investigate(instanceId):
-    for round_ in sorted(db.rounds.find({'_groupId': instanceId}), key=lambda x: x['timestamp']):
-        print 'Round: ' + str(round_['roundIndex'])
-        print 'User: ' + round_['userId']
-        print 'Timestamp: ' + str(round_['timestamp'])
-        print 'Payoff: ' + str(round_.get('payoff'))
-        print
 
 
 def plotOne():
     coops = coopWrapper()
-    gameCoops = [[mean(coops.get(instanceId, [1])) for instanceId in games] for games in pilotGames]
-    return [mean(x) for x in gameCoops]
+    games = [game['instances'] for game in sorted(db.gamegroups.find(), key=lambda x: x['counter'])]
+    gameCoops = [[mean(coops.get(instanceId, [1])) for instanceId in games] for games in games]
+    y = [mean(x) for x in gameCoops]
+    plt.plot(range(1,21), y)
+    plt.ylabel('Fraction of Cooperation')
+    plt.xlabel('Game counter')
+    plt.xlim((1,20))
+    plt.xticks(range(1,21))
+    plt.show()
+
 
 def plotTwo():
+    games = [game['instances'] for game in sorted(db.gamegroups.find(), key=lambda x: x['counter'])]
     roundCoops = [[] for _ in range(10)]
     coops = coopWrapper()
-    for games in pilotGames:
+    for games in games:
         gameGroupRoundCoop = [[] for _ in range(10)]
         for instanceId in games:
             roundCoop = coops.get(instanceId, numpy.ones(10))
@@ -72,11 +52,20 @@ def plotTwo():
         gameGroupRoundCoop = [mean(x) for x in gameGroupRoundCoop]
         for roundIndex, l in enumerate(roundCoops):
             roundCoops[roundIndex].append(gameGroupRoundCoop[roundIndex])
-    return roundCoops
+    y1 = [mean(x[:11]) for x in roundCoops]
+    y2 = [mean(x[11:]) for x in roundCoops]
+    plt.plot(range(1,11), y1, label='Games 1-10')
+    plt.plot(range(1,11), y2, label='Games 11-20')
+    plt.ylabel('Fraction of Cooperation')
+    plt.xlabel('Round')
+    plt.xlim((1,10))
+    plt.xticks(range(1,11))
+    plt.legend()
+    plt.show()
 
 
-            
-        
+
+
 def coopWrapper():
     instances = list(db.ts.experiments.find())
     coops = {}
@@ -87,6 +76,7 @@ def coopWrapper():
             coops[instanceId] = gameCoop(instanceId)
     return coops
 
+
 def gameCoop(instanceId):
     roundObjs = sorted(db.rounds.find({'_groupId': instanceId}), key=lambda x: x['roundIndex'])
     rounds = [list(actions) for _, actions in itertools.groupby(roundObjs, key=lambda x: x['roundIndex'])]
@@ -96,12 +86,13 @@ def gameCoop(instanceId):
         roundCoop.append(float(coops)/2)
     return roundCoop
 
+
 def payment():
-    games = list(db.ts.experiments.find())
+    instances = list(db.ts.experiments.find())
     payoffs = []
-    for game in games:
-        users = game['users']
-        rounds = sorted(list(db.rounds.find({'_groupId': game['_id']})), key = lambda x: x['timestamp'])
+    for instance in instances:
+        users = instance['users']
+        rounds = sorted(list(db.rounds.find({'_groupId': instance['_id']})), key = lambda x: x['timestamp'])
         user1_rounds = filter(lambda x: x['userId'] == users[0] and 'payoff' in x, rounds)
         user2_rounds = filter(lambda x: x['userId'] == users[1] and 'payoff' in x, rounds)
         if len(user1_rounds + user2_rounds) == 20:
@@ -110,7 +101,10 @@ def payment():
             user1_payoff = sum([x['payoff'] for x in user1_rounds])
             user2_payoff = sum([x['payoff'] for x in user2_rounds])
             payoffs.extend([user1_payoff, user2_payoff])
+        else:
+            print rounds
     return payoffs
+
 
 def timing():
     games = list(db.ts.experiments.find())
@@ -125,9 +119,23 @@ def timing():
     print max(start_times)
     print 'Overall length: %s' % overall_delta
 
+
 def acceptances():
     assignments = db.ts.assignments.find()
     accepts = [item['acceptTime'] for item in assignments]
     print min(accepts)
     print max(accepts)
     
+
+def investigate(instanceId):
+    for round_ in sorted(db.rounds.find({'_groupId': instanceId}), key=lambda x: x['timestamp']):
+        print 'Round: ' + str(round_['roundIndex'])
+        print 'User: ' + round_['userId']
+        print 'Action: ' + str(round_['action'])
+        print
+
+
+def randomInst():
+    instances = list(db.ts.experiments.find())
+    index = random.randint(0, len(instances))
+    return instances[index]['_id']
