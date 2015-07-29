ea849dea75779cb5174c26bd8649c9cc722dea06
Lili Dworkin <lilidworkin@gmail.com>
2015-07-29 16:07:25 -0400

    publish games and bonus

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-Meteor.publish('users', function() { return Meteor.users.find(); });
+Meteor.publish('users', function() { return Meteor.users.find({}, {fields:{games:1, bonus:1}}); });
 Meteor.publish('rounds', function() { return Rounds.find(); });
 Meteor.publish('actions', function() { return Actions.find(); });
 Meteor.publish('games', function() { return Games.find(); });
