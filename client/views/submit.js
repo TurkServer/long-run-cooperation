0ec4a53107bb22979549ca8b39a6f0936ff9a543
Lili Dworkin <lilidworkin@gmail.coom>
2015-07-29 08:08:04 -0400

    store games, bonus on user object

--- client/views/submit.js
+++ client/views/submit.js
@@ -1,6 +1,7 @@
 Template.submit.events({
     "submit .survey": function(e) {
 	e.preventDefault();
+	Meteor.call('submitHIT');
 	var results = {'confusing': e.target.free.value}
 	TurkServer.submitExitSurvey(results);
     },
