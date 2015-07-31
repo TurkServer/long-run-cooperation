7bb24bb5af160842c1300e3106c31391d3c4946e
Lili Dworkin <lilidworkin@gmail.coom>
2015-07-30 20:34:11 -0400

    assign qual script

--- server/scripts.js
+++ server/scripts.js
@@ -31,4 +31,44 @@ Meteor.methods({
 	    asstObj.payBonus('Bonus for the decision-making HIT.');
 	});
     },
+    'grantQuals': function(qualId) {
+	TurkServer.checkAdmin();
+	console.log('grantQuals');
+	var batchId = Batches.findOne({name: 'recruiting'})._id;
+	var quals = 0;
+	Assignments.find({
+	    batchId: batchId,
+	    status: 'completed',
+	    'exitdata.participation': {$exists: true}
+	}).forEach(function(asst) {
+	    var asstObj = TurkServer.Assignment.getAssignment(asst._id);
+	    var worker = Workers.findOne({_id: asst.workerId});
+	    if (worker && !worker.contact) {
+		// should he be included?
+		console.log(worker._id + " didn't check contact box, but said: " + asst.exitdata.participation);
+	    }
+	    //TurkServer.Util.assignQualification(asst.workerId, qualId, 1, false)
+	    quals += 1;
+	});
+	console.log("Quals granted: " + quals);
+    },
+    'chooseSecondTime': function() {
+	TurkServer.checkAdmin();
+	console.log('chooseTime');
+	var times = {};
+	Workers.find().forEach(function(worker) {
+	    var workerTimes = worker.available.times;
+	    if (workerTimes.indexOf('12 -0500') == -1 &&
+		workerTimes.indexOf('13 -0500') == -1) {
+		_.each(workerTimes, function(time) {
+		    if (time in times) {
+			times[time] += 1;
+		    } else {
+			times[time] = 1;
+		    }
+		});
+	    }
+	});
+	console.log(times);
+    }
 });
