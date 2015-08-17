a3f97ba346e9b8d95e0d8108e46523d77f91b115
Lili Dworkin <lilidworkin@gmail.com>
2015-08-17 14:31:25 -0400

    fixing indentation

--- server/assigner.js
+++ server/assigner.js
@@ -1,71 +1,71 @@
 var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
-  hasProp = {}.hasOwnProperty;
+hasProp = {}.hasOwnProperty;
 
 TurkServer.Assigners.PairAssigner = (function(superClass) {
-  extend(PairAssigner, superClass);
+    extend(PairAssigner, superClass);
 
-  function PairAssigner() {
-    return PairAssigner.__super__.constructor.apply(this, arguments);
-  }
+    function PairAssigner() {
+	return PairAssigner.__super__.constructor.apply(this, arguments);
+    }
 
-  PairAssigner.prototype.initialize = function() {
-      PairAssigner.__super__.initialize.apply(this, arguments);
+    PairAssigner.prototype.initialize = function() {
+	PairAssigner.__super__.initialize.apply(this, arguments);
 
-      this.counter = this.setCounter();
+	this.counter = this.setCounter();
 
-      this.lobby.events.on("reset-lobby", (function(_this) {
-	  return function() {
-	      _this.counter = 0;
-	      console.log('Reset counter.');
-	  }
-      })(this));
+	this.lobby.events.on("reset-lobby", (function(_this) {
+	    return function() {
+		_this.counter = 0;
+		console.log('Reset counter.');
+	    }
+	})(this));
 
-      this.lobby.events.on("exit-survey", (function(_this) {
-	  return function() {
-	      var lobbyAssts = _this.lobby.getAssignments();
-	      for (var i=0; i<lobbyAssts.length; i++) {
-		  var asst = lobbyAssts[i];
-		  _this.lobby.pluckUsers([asst.userId]);
-		  asst.showExitSurvey();
-	      }
-	  }
-      })(this));
+	this.lobby.events.on("exit-survey", (function(_this) {
+	    return function() {
+		var lobbyAssts = _this.lobby.getAssignments();
+		for (var i=0; i<lobbyAssts.length; i++) {
+		    var asst = lobbyAssts[i];
+		    _this.lobby.pluckUsers([asst.userId]);
+		    asst.showExitSurvey();
+		}
+	    }
+	})(this));
 
-      this.lobby.events.on("next-game", (function(_this) {
-	  return function() {
-	      assignFunc(_this);
-	  };
-      })(this));
+	this.lobby.events.on("next-game", (function(_this) {
+	    return function() {
+		assignFunc(_this);
+	    };
+	})(this));
 
-      this.lobby.events.on("end-games", (function(_this) {
-	  return function() {
-	      endGamesFunc(_this);
-	  };
-      })(this));
+	this.lobby.events.on("end-games", (function(_this) {
+	    return function() {
+		endGamesFunc(_this);
+	    };
+	})(this));
 
-  };
+    };
     
- 
-  PairAssigner.prototype.setCounter = function() {
-      var assts = Assignments.find({status: 'assigned'}).fetch();
-      if (assts.length == 0) { return 0; }
-      var counts = _.map(assts, function(asst) {
-	  return (asst.instances && asst.instances.length) || 0;
-      });
-      return _.max(counts);
-  }
+    
+    PairAssigner.prototype.setCounter = function() {
+	var assts = Assignments.find({status: 'assigned'}).fetch();
+	if (assts.length == 0) { return 0; }
+	var counts = _.map(assts, function(asst) {
+	    return (asst.instances && asst.instances.length) || 0;
+	});
+	return _.max(counts);
+    }
 
-  PairAssigner.prototype.userJoined = function(asst) {
-      if (asst.getInstances().length > 0) {
-	  LobbyStatus.update({_id: asst.userId}, {$set: {status: true}});
-      }
-      if (this.counter >= numGames) { 
-	  this.lobby.pluckUsers([asst.userId]);
-	  asst.showExitSurvey();
-      }
-  };
+    PairAssigner.prototype.userJoined = function(asst) {
+	if (asst.getInstances().length > 0) {
+	    LobbyStatus.update({_id: asst.userId}, {$set: {status: true}});
+	}
+	if (this.counter >= numGames) { 
+	    this.lobby.pluckUsers([asst.userId]);
+	    asst.showExitSurvey();
+	}
+    };
 
-  return PairAssigner;
+    return PairAssigner;
 
 })(TurkServer.Assigner);
 
@@ -119,21 +119,21 @@ function assignFunc(_this) {
 	var instances = [];
 	var leftOut = [];
 	for (var key in pairs) {
-		var assts = pairs[key];
-		if (assts.length == 2) {
-			var instance = _this.batch.createInstance(['main'])
-			instance.setup();
-			instances.push(instance.groupId);
-			for (var i=0; i<2; i++) {
-				var asst = assts[i];
-				_this.lobby.pluckUsers([asst.userId]);
-				instance.addAssignment(asst);
-			}
-		} else {
-			leftOut.push(pairs[key][0].asstId);
+	    var assts = pairs[key];
+	    if (assts.length == 2) {
+		var instance = _this.batch.createInstance(['main'])
+		instance.setup();
+		instances.push(instance.groupId);
+		for (var i=0; i<2; i++) {
+		    var asst = assts[i];
+		    _this.lobby.pluckUsers([asst.userId]);
+		    instance.addAssignment(asst);
 		}
-		// Allow server to catch up
-		sleep(100);
+	    } else {
+		leftOut.push(pairs[key][0].asstId);
+	    }
+	    // Allow server to catch up
+	    sleep(100);
 	}
 	GameGroups.insert({
 	    'counter': _this.counter,
