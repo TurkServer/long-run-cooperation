f6c29e8303f563b9787eadb5879366a2a2e8be5b
Lili Dworkin <lilidworkin@gmail.com>
2015-08-17 14:04:23 -0400

    adding line breaks

--- punishers.py
+++ punishers.py
@@ -18,6 +18,7 @@ batches = sorted(batchMap.keys())
 
 defaultBatch = batches[-1]
 
+
 def investigate(batch=defaultBatch):
     batchId = batchMap[batch]
     for exp in db.ts.experiments.find({'batchId': batchId}):
@@ -28,6 +29,7 @@ def investigate(batch=defaultBatch):
 def findWorkerId(userId):
     user = db.users.find_one({'_id': userId})
     return user['workerId']
+
 
 def userGames(workerId, batch=defaultBatch):
     batchId = batchMap[batch]
@@ -53,6 +55,7 @@ def printGame(exp):
             if len(actions) == 1:
                 action = actions[0]
                 print 'Round %d: %s - %d' % (round_['index'], action['userId'], action['action'])
+
 
 def printRound(index, user0, user0action, user1, user1action, delta):
     print 'Round %d: %s - %d, %s - %d (%s s)' % (index, user0, user0action, user1, user1action, delta.seconds)
