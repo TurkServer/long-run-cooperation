d4768f6e0ec17e9c98af3c6a7f4f45e9312d29dd
Andrew Mao <mizzao@gmail.com>
2015-08-18 17:11:14 -0400

    Edits and tweaks to exit survey

--- client/views/survey.js
+++ client/views/survey.js
@@ -1,8 +1,43 @@
+var bins = [
+	[0],
+	[1, 4],
+	[5, 9],
+	[10, 49],
+	[50, 99],
+	[100]
+];
+
+Template.numberRadioButtons.helpers({
+	options: bins.map(function(bin) {
+		if (bin[0] === 0) {
+			return {
+				value: 0,
+				caption: "none"
+			}
+		}
+		else if (bin.length === 1) {
+			return {
+				value: bin[0] + "+",
+				caption: bin[0] + " or more"
+			}
+		}
+		else {
+			return {
+				value: bin[0] + "-" + bin[1],
+				caption: bin[0] + " to " + bin[1]
+			}
+		}
+	}),
+	attributes: function() {
+		return Template.parentData();
+	}
+});
+
 Template.exitsurvey.events({
     "submit .survey": function(e, tmpl) {
 	e.preventDefault();
 	var form = e.target;
-	var gender = tmpl.find("input[name=GenderOptions]:checked");
+	var gender = tmpl.find("select[name=gender]").value;
 	var pd = tmpl.find("input[name=PD]:checked");
 	var pg = tmpl.find("input[name=PG]:checked");
 	results = {"age": form.age.value,
@@ -19,7 +54,9 @@ Template.exitsurvey.events({
 		   "learn_others": form.learn_others.value,
 		   "continued": form.continued.value,
 		   "compensation": form.compensation.value,
-		   "future": form.future.value}
+		   "future": form.future.value,
+		"misc": form.misc.value
+	};
 	TurkServer.submitExitSurvey(results);
     }
 });
