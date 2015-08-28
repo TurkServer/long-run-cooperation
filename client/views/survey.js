f96b5b03323bdb3d724487454ec49c08f3866c45
Lili Dworkin <lilidworkin@gmail.com>
2015-08-28 16:39:48 -0400

    final exit survey tweaks

--- client/views/survey.js
+++ client/views/survey.js
@@ -1,36 +1,36 @@
 var bins = [
-	[0],
-	[1, 4],
-	[5, 9],
-	[10, 49],
-	[50, 99],
-	[100]
+    [0],
+    [1, 4],
+    [5, 9],
+    [10, 49],
+    [50, 99],
+    [100]
 ];
 
 Template.numberRadioButtons.helpers({
-	options: bins.map(function(bin) {
-		if (bin[0] === 0) {
-			return {
-				value: 0,
-				caption: "none"
-			}
-		}
-		else if (bin.length === 1) {
-			return {
-				value: bin[0] + "+",
-				caption: bin[0] + " or more"
-			}
-		}
-		else {
-			return {
-				value: bin[0] + "-" + bin[1],
-				caption: bin[0] + " to " + bin[1]
-			}
-		}
-	}),
-	attributes: function() {
-		return Template.parentData();
+    options: bins.map(function(bin) {
+	if (bin[0] === 0) {
+	    return {
+		value: 0,
+		caption: "none"
+	    }
 	}
+	else if (bin.length === 1) {
+	    return {
+		value: bin[0] + "+",
+		caption: bin[0] + " or more"
+	    }
+	}
+	else {
+	    return {
+		value: bin[0] + "-" + bin[1],
+		caption: bin[0] + " to " + bin[1]
+	    }
+	}
+    }),
+    attributes: function() {
+	return Template.parentData();
+    }
 });
 
 Template.exitsurvey.events({
@@ -43,7 +43,6 @@ Template.exitsurvey.events({
 	var pd_games = tmpl.find("input[name=PD_games]:checked");
 	var pg_games = tmpl.find("input[name=PG_games]:checked");
 	var contact = tmpl.find("input[name=contact]:checked");
-	console.log(gender)
 	results = {"age": form.age.value,
 		   "gender": gender && gender.value,
 		   "occupation": form.occupation.value,
@@ -58,14 +57,14 @@ Template.exitsurvey.events({
 		   "other_strategies_time": form.other_strategies_time.value,
 		   "learn_yourself": form.learn_yourself.value,
 		   "learn_others": form.learn_others.value,
-				"norms": form.norms.value,
-				"continued": form.continued.value,
-				"discuss": form.discuss.value,
+		   "norms": form.norms.value,
+		   "continued": form.continued.value,
+		   "discuss": form.discuss.value,
 		   "compensation": form.compensation.value,
 		   "future": form.future.value,
 		   "contact": contact && contact.value,
 		   "misc": form.misc.value
-	};
+		  };
 	Meteor.call('setSurveyBonus');
 	TurkServer.submitExitSurvey(results);
     }
